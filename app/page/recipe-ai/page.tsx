0ef776741b0d16a17bd7d493ce6cb4d5325d72de
SakagamiTaichi <144244146+SakagamiTaichi@users.noreply.github.com>
2025-01-02 12:49:16 +0900

    Vercelデプロイ時にフロントエンド側でバックエンドURLを指定しているが、まだ一度もデプロイに成功していないため、フロント側からAPIにアクセスできずデプロイエラーが発生するため、一度ダミーデータでデプロイする

--- app/page/recipe-ai/page.tsx
+++ app/page/recipe-ai/page.tsx
@@ -1,17 +1,52 @@
+//app/page/recipe-ai/page.tsx
 import ClientRecipeSearch from "../../components/ui/RecipeSearch";
 import { Ingredient } from "../../types";
 
 async function getIngredients(): Promise<Ingredient[]> {
-  const url = process.env.BACKEND_API_URL + "/ingredient/readall";
-  if (!url) {
-    throw new Error("BACKEND_API_URL is not defined");
-  }
-  const response = await fetch(url, {
-    next: {
-      revalidate: 3600, // 1時間キャッシュ
+  // const url = process.env.BACKEND_API_URL + "/ingredient/readall";
+  // if (!url) {
+  //   throw new Error("BACKEND_API_URL is not defined");
+  // }
+  // const response = await fetch(url, {
+  //   next: {
+  //     revalidate: 3600, // 1時間キャッシュ
+  //   },
+  // });
+  // const data = await response.json();
+
+  // ダミーのデータを返す
+  const data = [
+    {
+      _id: "1",
+      name: "キャベツ",
+      category: "野菜",
     },
-  });
-  const data = await response.json();
+    {
+      _id: "2",
+      name: "牛肉",
+      category: "肉",
+    },
+    {
+      _id: "3",
+      name: "卵",
+      category: "卵・乳製品",
+    },
+    {
+      _id: "4",
+      name: "牛乳",
+      category: "卵・乳製品",
+    },
+    {
+      _id: "5",
+      name: "小麦粉",
+      category: "穀物",
+    },
+    {
+      _id: "6",
+      name: "砂糖",
+      category: "その他",
+    },
+  ];
 
   return data.map(
     (ingredient: { _id: string; name: string; category: string }) => ({
